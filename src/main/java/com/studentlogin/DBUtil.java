fde51571aba9a9f9d34689eb70318af71335b7ac
Steffinald <inboxsteffinald@gmail.com>
2025-09-24 15:40:58 +0530

    Updated classpath,war file,DBUtil file

--- src/main/java/com/studentlogin/DBUtil.java
+++ src/main/java/com/studentlogin/DBUtil.java
@@ -5,12 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String url = "jdbc:mysql://localhost:3306/rusi";
-    private static final String username = "root";
-    private static final String password = "Steffin@2005";
+	private static final String db_URL = "jdbc:postgresql://ep-muddy-lake-adqrejdw-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"; 
+    private static final String USER = "neondb_owner";  
+    private static final String PASSWORD = "npg_zelQ3Ucvx5Xs";  
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(url, username, password);
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver"); // optional for JDBC 4+
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL Driver not found", e);
+        }
+        return DriverManager.getConnection(db_URL, USER, PASSWORD);
     }
 }
